2bfad3400d9f1e1e81b759d927fb8b6f68de08f4
F-Saravia <contact.fsaravia@gmail.com>
2024-02-16 22:35:21 +0100

    filter system: styles + js logic (interactions + dom-updaters)

--- dom-interactions/index.js
+++ dom-interactions/index.js
@@ -1,7 +1,82 @@
 async function onPageLoad() {
+    await onLoad_updatePortfolioHeader(); //depends if user is logged in or not. (index-updaters.js)
     await helper_updatePortfolioGallery(); //fetch from api works to display. (index-updaters.js)
 }
 
 (async function () {
     await onPageLoad();
+
+    // page-header, login/logout button: onclick
+    document.getElementById("login-logout").addEventListener("click", event => {
+        event.preventDefault();
+        if (UsersManager.isLogged()) {
+            UsersManager.logout();
+            window.location.reload()
+        }
+        else {
+            window.location.replace("./login.html");
+        }
+    })
+
+    /* Portfolio section interactions
+     *
+     * Portfolio-Header:
+     * 
+     *      1- user is logged-in:
+     *          button 'Modifer' => interaction: display gallery-modal
+     * 
+     *      2- user is logged-out:
+     *          filter buttons => interaction: update displayed works
+     */
+
+    // 1- 'Modifier' => display gallery-modal
+    // due to loading operations at the appearance of the gallery-modal
+    // this interaction allowing to show the modal it's located at modals.js => /***** Gallery Modal *****/
+
+    // 2- Filter buttons
+    if (!UsersManager.isLogged()) {
+        //instance to keep track of selected categories (class definition at index-updaters.js)
+        const filtersState = new FiltersState()
+
+        //dom elements: filter buttons (tous + categories buttons)
+        const filterBtn_Tous = document.querySelector(".filter-all-categories");
+        const filterBtns_Categories = document.querySelectorAll(".filter-category-value");
+
+        //initialize "Tous" button with 'button-green' because by default filter buttons are set to 'button-white'
+        if (filtersState.none_or_all_categories_are_selected()) {
+            helper_setFilterButtonState(filterBtn_Tous, "green");
+        }
+
+        //INTERACTION: Button "Tous"
+        filterBtn_Tous.addEventListener("click", () => {
+            const categoryId = parseInt(filterBtn_Tous.value);
+            filtersState.toggleCategory(categoryId);
+            helper_setFilterButtonState(filterBtn_Tous, "green");
+
+            filterBtns_Categories.forEach((categoryBtn) => {
+                helper_setFilterButtonState(categoryBtn, "white");
+            });
+        })
+
+        //INTERACTION: each category filter-button
+        filterBtns_Categories.forEach((categoryBtn) => {
+            const categoryId = parseInt(categoryBtn.value);
+            categoryBtn.addEventListener("click", () => {
+                filtersState.toggleCategory(categoryId);
+                helper_toggleFilterButtonState(categoryBtn);
+
+                //update 'Tous' if necessary
+                if (filtersState.none_or_all_categories_are_selected()) {
+                    filtersState.toggleCategory(-1);  // -1 is the id of "Tous" category
+                    helper_setFilterButtonState(filterBtn_Tous, "green");
+                    filterBtns_Categories.forEach((button) =>
+                        helper_setFilterButtonState(button, "white")
+                    );
+                } else {
+                    helper_setFilterButtonState(filterBtn_Tous, "white");
+                }
+            })
+
+        })
+    }
 })()
--- dom-updaters/index-updaters.js
+++ dom-updaters/index-updaters.js
@@ -1,5 +1,3 @@
-
-
 /************************
 *** PORTFOLIO SECTION ***
 ************************/
@@ -7,7 +5,45 @@
 // -------------------
 // PORTFOLIO SECTION : helper functions for the portfolio section
 
-// --- PORTFOLIO SECTION, GALLERY : cards & populate helper functions ---
+// --- (1) PORTFOLIO SECTION, HEADER : filter buttons helper functions ---
+
+function helper_createFilterButton(jsonCategory) {
+    const { id, name } = jsonCategory;
+    let filterButton = document.createElement("button");
+    filterButton.className = "button-large button-white"
+    filterButton.type = "button"
+    filterButton.innerText = name;
+    filterButton.value = id;
+    filterButton.ariaLabel = `Bouton pour montrer les projets de la catégorie ${name}`
+
+    if (id === -1) {
+        filterButton.classList.add("filter-all-categories")
+    } else {
+        filterButton.classList.add("filter-category-value")
+    }
+
+    return filterButton
+}
+
+function helper_setFilterButtonState(btn, state) {
+    if (state === "green") {
+        btn.classList.add("button-green");
+        btn.classList.remove("button-white");
+        return;
+    }
+    if (state === "white") {
+        btn.classList.add("button-white");
+        btn.classList.remove("button-green");
+        return;
+    }
+}
+
+function helper_toggleFilterButtonState(btn) {
+    btn.classList.toggle("button-white");
+    btn.classList.toggle("button-green");
+}
+
+// --- (2) PORTFOLIO SECTION, GALLERY : cards & populate helper functions ---
 
 function helper_createPortfolioWorkCard(jsonWork) {
     const { title, imageUrl, category } = jsonWork;
@@ -43,6 +79,108 @@ async function helper_updatePortfolioGallery() {
     }
 }
 
+
+
+// helper class to store/remove selected filters then trigger filtering function then update the gallery
+class FiltersState {
+    #selectedFilters
+
+    constructor() {
+        this.#selectedFilters = [];
+    }
+
+    #getWorks_FilteredByCategories() {
+        return WorksManager.getWorks()
+            .then(
+                works => {
+                    return this.none_or_all_categories_are_selected()
+                        ? works
+                        : works.filter(
+                            work => this.#selectedFilters.includes(work.categoryId)
+                        )
+                }
+            )
+    }
+
+    #updateFilteredGallery() {
+        this.#getWorks_FilteredByCategories().then(
+            filteredWorks => {
+                helper_populatePortfolioGallery(filteredWorks)
+            }
+        )
+    }
+
+    toggleCategory(categoryID) {
+        //N.B: -1 corresponds to "Tous" button
+        if (categoryID === -1) {
+            this.#selectedFilters = [];
+        } else {
+            //removes the category
+            if (this.#selectedFilters.includes(categoryID)) {
+                this.#selectedFilters.splice(this.#selectedFilters.indexOf(categoryID), 1);
+            }
+            //adds the category
+            else {
+                this.#selectedFilters.push(categoryID)
+            }
+        }
+
+        this.#updateFilteredGallery();
+    }
+
+    none_or_all_categories_are_selected() {
+        return this.#selectedFilters.length === 0 || this.#selectedFilters.length === CategoriesManager.countAllCategories;
+    }
+}
 // --- END PORTFOLIO SECTION : helpers-functions and helper-class ---
 
+// -------------------
+// PORTFOLIO SECTION : Header Updates
+
+async function onLoad_updatePortfolioHeader() {
+    let portfolioHeader = document.querySelector(".portfolio__header");
+
+    // Header-case: user logged-in => Button 'Modifier' instead of filter buttons
+    if (UsersManager.isLogged()) {
+        portfolioHeader.classList.add("user-logged-in");
+        portfolioHeader.classList.remove("user-logged-out");
+        const btnModify = document.createElement("button");
+        btnModify.type = "button"
+        btnModify.id = "show-gallery-modal";
+        btnModify.className = "button-small";
+        btnModify.ariaLabel = "Bouton pour gérer l'ensemble des projets (ajouter ou supprimer des projets)"
+        btnModify.innerHTML = `
+        <i class="fa-regular fa-pen-to-square"></i>
+        modifier
+        `;
+        portfolioHeader.appendChild(btnModify);
+    }
+
+    // Header-case: user logged-out : Filters buttons ('Tous' + each category) instead of button 'Modifier'
+    else {
+        //Container  for filter buttons
+        portfolioHeader.classList.add("user-logged-out");
+        portfolioHeader.classList.remove("user-logged-in");
+        const filtersContainer = document.createElement("div");
+        filtersContainer.classList.add("filters-container");
+
+        //Button: 'Tous'
+        const buttonTous = helper_createFilterButton({ id: -1, name: "Tous" })
+        filtersContainer.appendChild(buttonTous)
+
+        //Buttons: categories.forEach(...)
+        let categories = await CategoriesManager.getCategories();
+        if (categories && Array.isArray(categories)) {
+            categories.forEach(category => {
+                filtersContainer.appendChild(
+                    helper_createFilterButton(category)
+                );
+            });
+        }
+        portfolioHeader.appendChild(filtersContainer);
+    }
+}
+// --- END PORTFOLIO SECTION : Header Updates ---
+
+// --- END PORTFOLIO SECTION : Gallery ---
 /*** END PORTFOLIO SECTION ***/
